4ea9c41190836c2c596097fb0c55f12e772bd1b7
dennysunny <iamdennysunny@gmail.com>
2026-05-17 01:18:32 +0530

    feat: update Supabase configuration to remove deprecated environment variables and add necessary keys

--- src/app/core/configs/supabase.config.ts
+++ src/app/core/configs/supabase.config.ts
@@ -1,6 +1,6 @@
 import { environment } from '../../../environments/environment';
 
 export const SUPABASE_CONFIG = {
-  url: import.meta.env['VITE_SUPABASE_URL'] || environment.SUPABASE_URL,
-  anonKey: import.meta.env['VITE_SUPABASE_ANON_KEY'] || environment.SUPABASE_ANON_KEY,
+  url: environment.SUPABASE_URL,
+  anonKey: environment.SUPABASE_ANON_KEY,
 } as const;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   API_SECREAT_KEY: '',
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: '',
+  SUPABASE_URL: ' https://krxebsedqgyjmookjrav.supabase.co',
+  SUPABASE_ANON_KEY:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtyeGVic2VkcWd5am1vb2tqcmF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NDM0NjgsImV4cCI6MjA5NDUxOTQ2OH0.4eetOvDf9-X-eF56VE82vmbJYGxkIlrP1EYKk4sFgeE',
 };
